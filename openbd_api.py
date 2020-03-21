b07b63ce459d275a2c4ce9e495196a19a204b3b0
nesosuke <nesosuke@gmail.com>
2020-03-22 01:39:33 +0900

    著者名取得

--- openbd_api.py
+++ openbd_api.py
@@ -5,7 +5,8 @@ import sys
 
 ISBN = sys.argv[1]
 response = req.get('https://api.openbd.jp/v1/get?isbn=' + str(ISBN)).json()
-BookInfo = response[0].get("onix")
-BookTitle = BookInfo.get("DescriptiveDetail").get("TitleDetail").get("TitleElement").get("TitleText").get("content")
-#BookAuthor = BookInfo["authors"][0]
+BookInfo = response[0].get("onix").get("DescriptiveDetail")
+BookTitle = BookInfo.get("TitleDetail").get("TitleElement").get("TitleText").get("content")
+BookAuthor = BookInfo.get("Contributor")[0].get("PersonName").get("content")
 print(BookTitle)
+print(BookAuthor)
