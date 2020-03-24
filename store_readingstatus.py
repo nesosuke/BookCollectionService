9318f3cc5cbc0c87909e45eca94199c81edcf17c
nesosuke <nesosuke@gmail.com>
2020-03-24 10:39:44 +0900

    データの格納なあ...検索しやすい方法がわからんがとりあえず案

--- store_readingstatus.py
+++ store_readingstatus.py
@@ -2,7 +2,8 @@
 # ISBNでDBを検索して、登録があるか探す
 # なければ新規作成、あればReadCountを1増やす
 # 初回登録は未読として、2回目以降の登録を既読とする
-# 検索キーはISBNにする、DB行のユニーク値として念の為にIDを発行する
+# 検索キーはISBNにするけど、別にユニークなIDを振っておきたい気もする（岩○文庫の例があるので）（？）
+
 
 import requests as req
 import sys
@@ -20,15 +21,21 @@ Published_at[1] = BookInfo.get("pubdate")[4:6]
 Published_at[2] = BookInfo.get("pubdate")[6:8]
 Cover = BookInfo.get("cover")
 
-BookInfo = [
-            Title, 
-            Author, 
-            Publisher, 
-            Published_at, 
-            Cover
-            ]
+BookInfo = {
+            "Title": Title,
+            "Author": Author,
+            "Publisher": Publisher,
+            "Publishded_at": Published_at, 
+            "Cover": Cover,
+            }
 
-ReadingData = [ISBN, BookInfo, ]
+ReadingStatus = {      # 「なければ新規作成」の部分をやりたい
+                "Read_at": Read_at, 
+                "ReadStatus": ReadStatus, 
+                "ReadCount": ReadCount, 
+                } 
+
+Data = [ISBN, BookInfo, ReadingStatus, response]
 
 print(Title)
 print(Author)
