5fd7841f6e276f5cf8cd3996485c3f6c79b27b3b
nesosuke <neso@neso.tech>
2020-07-15 19:32:29 +0900

    v0.0.1のために整理

--- botsu/bookapi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import requests as req
-import sys
-
-if __name__ == '__main__':
-    ISBN = sys.argv[1]
-    response = req.get('https://api.openbd.jp/v1/get?isbn=' + str(ISBN)).json()[0]
-    BookInfo = response.get("summary")
-    return BookInfo
