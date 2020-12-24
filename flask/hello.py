ec680cfe91e16917539b9c8617721fe8073bca34
nesosuke <nesosuke@gmail.com>
2020-12-25 05:50:39 +0900

    ISBN to title, author, publisher の検索部分できた

--- flask/hello.py
+++ flask/hello.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -15,15 +16,22 @@ def index():
 def result():
     query = request.args.get('isbn')
     result = getinfo.search(query)
+
     if result is "Error":
-        title = "ERR: Bookinfo not found"
+        error = "ERR: Bookinfo not found"
+        title = error
+        bookauthor = error
+        publisher = error
     else:    
         title= result['title']
-
+        bookauthor = result['author']
+        publisher = result['publisher']
     return render_template(
         'search.html',
         title="search result",
         booktitle=title,
+        bookauthor=bookauthor,
+        publisher=publisher,
     )
 
 if __name__ == "__main__":
