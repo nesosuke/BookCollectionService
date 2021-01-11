0d2512daf5f36f2a80faed6679799eb667030c42
nesosuke <neso@neso.tech>
2021-01-11 12:05:46 +0900

    bookdb_update: listが入力されたときの処理 文字列検索をするとsearcNDLがlistを返すので，何もしないように変更

--- flask/bookDB.py
+++ flask/bookDB.py
@@ -21,33 +21,37 @@ def isISBN(query):
         return False
 
 
-def searchNDL(query, forceStrSearch=False, count=3):
+def searchNDL(query, stringsearch=False, count=5):
     query = str(query)
-    if isISBN(query) is False or forceStrSearch is True:
+    if isISBN(query) is False or stringsearch is True:
         url = 'https://iss.ndl.go.jp/api/opensearch?' \
             + 'cnt=' + str(count) + '&' \
             + 'title=' + str(query)
         res = req.get(url, verify=False)
         res = BeautifulSoup(res.content, 'lxml')
-        tmp =[]
+        tmp = []
         for i in range(count):
             tmp.append(res.channel.find('item'))
-        res = tmp
+        res = tmp  # type(res) == list
+
     elif isISBN(query) is True:
         url = 'https://iss.ndl.go.jp/api/opensearch?' \
             + 'isbn=' + str(query)
         res = req.get(url, verify=False)
+        # type(res) == dict
         res = BeautifulSoup(res.content, 'lxml',
                             from_encoding='uft-8').channel.find('item')
-        
     else:
         res = None
     return res
 
 
-def bookdb_update(query):
+def bookdb_update(query, skipsearch=False):
     res = searchNDL(query)
-    if res is not None or type(res) != list:
+    if type(res) == list:
+        return None
+
+    if res is not None or skipsearch is True:
         title = res.find('dc:title').text
         author = res.find('dc:creator').text
         publisher = res.find('dc:publisher').text
@@ -56,10 +60,14 @@ def bookdb_update(query):
             volume = res.find('dcndl:volume').text
         else:
             volume = '1'
+        # want fix ---
+        # 'AttributeError: 'NoneType' object has no attribute 'text''
+        # series = (res.find('dcndl:seriestitle') or None).text
         if res.find('dcndl:seriestitle') is not None:
             series = res.find('dcndl:seriestitle').text
         else:
-            series = 'None'
+            series = None
+        # ---
         permalink = res.find('guid').text
 
         mongo.db.bookdb.find_one_and_update(
@@ -82,13 +90,15 @@ def bookdb_update(query):
 
 def bookdb(query):
     if query is None:
-        data = 'query is None'
+        # data = 'query is None'
+        data = None
     elif isISBN(query) is not True:
         data = None
     else:  # 書籍情報があるか(MongoDB -> NDL)
         data = mongo.db.bookdb.find_one({'isbn': query})
         if data is None and bookdb_update(query) is None:
-            data = 'bookinfo is None'  # NDLでも見つからない場合
+            data = None  # NDLでも見つからない場合
+            # data = 'data is None'
         else:
             data = mongo.db.bookdb.find_one({'isbn': query})
     return data
@@ -96,4 +106,4 @@ def bookdb(query):
 
 if __name__ == "__main__":
     q = sys.argv[1]
-    print(searchNDL(q))
+    print(searchNDL(q)[0].find('dc:title').text)
