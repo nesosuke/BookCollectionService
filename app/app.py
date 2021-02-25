fd4c46e1d8f082f97017745c17717ad2d58645ab
nesosuke <neso@neso.tech>
2021-02-26 01:13:36 +0900

    app.route('/') の追加 	modified:   app.py

--- app/app.py
+++ app/app.py
@@ -10,27 +10,42 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/bookmeter"
 mongo = PyMongo(app)
 
 
-@app.route('/book')
+@app.route('/')
+def toppage():
+    return render_template('index.html')
+
+
+@app.route('/book', methods=['GET'])
 def show_bookinfo():
-    isbn = request.args.get('q')
-    bookinfo = modules.find_bookinfo_byisbn(isbn)  # dict
-    return render_template('bookinfo.html',
-                       bookinfo = bookinfo)
+    if request.method == 'GET':
+        isbn = request.args.get('q')
+        bookinfo = json.dumps(modules.find_bookinfo_byisbn(isbn))  # json
+        return render_template('bookinfo.html',
+                               bookinfo=bookinfo)
 
 
-@ app.route('/search', methods = ['GET'])
+@app.route('/search', methods=['GET'])
 def find_and_show_bookinfo():
-    query=request.args.get('q')
+    query = request.args.get('q')
     if query is None:
         return render_template('search.html')
     elif modules.isISBN13(query):
-        bookinfo=[modules.find_bookinfo_byisbn(query)]  # list
+        bookinfo = [modules.find_bookinfo_byisbn(query)]  # list
     else:
-        bookinfo=modules.find_bookinfo_bytitle(query)  # list
+        bookinfo = modules.find_bookinfo_bytitle(query)  # list
 
     return render_template('result.html',
-                           bookinfo = bookinfo)
+                           bookinfo=bookinfo)
+
+
+@app.route('/status', methods=['POST'])
+def record_status():
+    uid = 'test'  # flaskのログインセッションID からとる
+    isbn = str(request.form.get('isbn'))
+    status = str(request.form.get('status'))
+    t = modules.update_status_intoDB(uid, isbn, status)
+    return 'hoge'
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
